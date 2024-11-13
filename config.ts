001676bd0fb07e6bbfb576afba6e9c8792c923e5
aburd <aaron.burdick@protonmail.com>
2024-11-13 23:04:38 +0900

    feat: setup config workflow

---
+++ config.ts
@@ -0,0 +1,12 @@
+function reportConfigError(msg: string) {
+  throw new Error(`error in config: ${msg}`)
+}
+
+const DB_PATH = Deno.env.get("DB_PATH") as string;
+if (!DB_PATH) {
+  reportConfigError('DB_PATH is required')
+}
+
+export {
+  DB_PATH
+}
