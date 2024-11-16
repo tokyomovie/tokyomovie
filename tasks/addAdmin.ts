71953f8c8b70d5338d4fa7cc8628e4a0efff90f5
aburd <aaron.burdick@protonmail.com>
2024-11-16 15:55:06 +0900

    build: add a simple way of adding admins to project

---
+++ tasks/addAdmin.ts
@@ -0,0 +1,34 @@
+import { getConnection } from "../database/db.ts"
+import * as user from "../database/query/user.ts"
+import { hashPassword } from "../utils/auth.ts"
+
+let confirmed = false;
+const userData = {
+  name: '',
+  email: '',
+  passwordHash: '',
+  role: 'admin' as user.Role,
+};
+
+while (!confirmed) {
+  userData.name = prompt('Enter name: ') ?? '';
+  userData.email = prompt('Enter email: ') ?? '';
+  userData.passwordHash = prompt('Enter password: ') ?? '';
+  confirmed = confirm(`Going to created a user with:
+    - name: ${userData.name}
+    - email: ${userData.email}
+    - password: ${userData.passwordHash}
+Is that OK?`.trim());
+}
+
+try {
+  using connection = getConnection();
+  const { db } = connection;
+  userData.passwordHash = await hashPassword(userData.passwordHash)
+  user.createUser(db, userData)
+  console.log('Successfully created admin user:', userData.name);
+  Deno.exit(0);
+} catch(e) {
+  console.error(`Could not create user`, e)
+  Deno.exit(1);
+}
