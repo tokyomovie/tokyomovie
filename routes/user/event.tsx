312ad4b9d80074d7f071c39b35983c7923d81d71
crista <cristascheuer@gmail.com>
2024-11-26 21:45:55 +0900

    (draft) event proof of concept

--- routes/user/event.tsx
+++ routes/user/event.tsx
@@ -1,5 +1,7 @@
 import { FreshContext, PageProps } from "$fresh/server.ts";
-import { State } from "../../types/request.ts";
+import { RequestState } from "../../types/request.ts";
+import Header from "../../components/Header.tsx";
+import ShowingIsland from "../../islands/ShowingIsland/ShowingIsland.tsx";
 
 export const handler = {
   GET(_req: Request, ctx: FreshContext<State>) {
@@ -14,10 +16,12 @@ type EventProps = {
 export default function Event(props: PageProps<EventProps>) {
   const { flash } = props.data || {};
   return (
-    <div class="size-full px-4 py-8 mx-auto bg-[#86efac]">
+    <div className="text-foreground bg-background h-screen">
+      <Header />
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         {flash && <p class={`p-2 text-${flash.type}`}>{flash.message}</p>}
-        <h1>Events</h1>
+        <h1 class="text-3xl text-primary">Events</h1>
+        <ShowingIsland />
       </div>
     </div>
   );
