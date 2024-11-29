6911a255a5ad40f79f34d5e8cca4319736d081c4
crista <cristascheuer@gmail.com>
2024-11-29 16:18:47 +0900

    added a border to posters

--- islands/movie/Movie.tsx
+++ islands/movie/Movie.tsx
@@ -1,35 +1,37 @@
 export interface MovieProps {
-  movieTitle: string;
-  releaseYear: number;
-  synopsis: string;
-  runtime: number;
-  genre: string;
-  poster: string;
+    movieTitle: string;
+    releaseYear: number;
+    synopsis: string;
+    runtime: number;
+    genre: string;
+    poster: string;
 }
 
 export default function Movie(
-  { movieTitle, releaseYear, synopsis, runtime, genre, poster }: MovieProps,
+    { movieTitle, releaseYear, synopsis, runtime, genre, poster }: MovieProps,
 ) {
-  return (
-    <>
-      <div class="px-3 flex flex-col">
-        <div class="my-2 p-2 shadow-block border border-highlight rounded">
-          <p>{releaseYear}</p>
-        </div>
-        <div class="my-2 p-2 shadow-block border border-highlight rounded">
-          <p>{runtime} mins</p>
-        </div>
-        <div class="my-2 p-2 shadow-block border border-highlight rounded">
-          <p>{genre}</p>
-        </div>
-      </div>
-      <div class="py-5 px-3">
-        <div class="py-2">
-          <h2 class="text-2xl text-error">{movieTitle}</h2>
-          <img class="p-5" src={poster} />
-          <p>{synopsis}</p>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div class="px-3 flex flex-col">
+                <div class="my-2 p-2 shadow-block border border-highlight rounded">
+                    <p>{releaseYear}</p>
+                </div>
+                <div class="my-2 p-2 shadow-block border border-highlight rounded">
+                    <p>{runtime} mins</p>
+                </div>
+                <div class="my-2 p-2 shadow-block border border-highlight rounded">
+                    <p>{genre}</p>
+                </div>
+            </div>
+            <div class="py-5 px-3">
+                <div class="py-2">
+                    <h2 class="text-2xl text-error">{movieTitle}</h2>
+                    <div class="p-4 m-3 rounded bg-white shadow-block border border-highlight">
+                        <img src={poster} />
+                    </div>
+                    <p>{synopsis}</p>
+                </div>
+            </div>
+        </>
+    );
 }
