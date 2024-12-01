7c94095bcf6c26505a0e721bfb73f0179d100c75
aburd <aaron.burdick@protonmail.com>
2024-12-01 19:44:28 +0900

    feat: service-email

---
+++ services/mail.ts
@@ -0,0 +1,34 @@
+import sgMail from "@sendgrid/mail";
+import { ADMIN_EMAIL_ADDRESS, SENDGRID_API_KEY } from "../config.ts";
+
+type EmailMessage = {
+  to: string;
+  subject: string;
+  text?: string;
+  html: string;
+};
+
+export interface MailClient {
+  send(message: EmailMessage): Promise<boolean>;
+}
+
+export function sendMail(client: MailClient, message: EmailMessage) {
+  client.send(message);
+}
+
+function createMailClientWithSendGrid(sgMail: sgMail.MailService): MailClient {
+  sgMail.setApiKey(SENDGRID_API_KEY);
+  return {
+    send: async (message) => {
+      const [res] = await sgMail.send({
+        ...message,
+        from: ADMIN_EMAIL_ADDRESS,
+      });
+      return res.statusCode < 300;
+    },
+  };
+}
+
+export function createMailClient() {
+  return createMailClientWithSendGrid(sgMail);
+}
