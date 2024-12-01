d97417547dc7e4fa104aab3a571ca050d58f0b8d
aburd <aaron.burdick@protonmail.com>
2024-12-01 22:25:38 +0900

    util: add new notFound response

--- utils/response/server.ts
+++ utils/response/server.ts
@@ -1,3 +1,12 @@
+export function notFound(
+  headers: Record<string, string> = {},
+): Response {
+  return new Response(null, {
+    headers,
+    status: 404,
+  });
+}
+
 export function redirect(
   path: string,
   headers: Record<string, string> = {},
